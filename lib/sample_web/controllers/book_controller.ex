615162d424442a495780725cfa85662e3b4c5208
Slavo <slavo2@users.noreply.github.com>
2018-08-04 00:04:21 +0200

    add query parameters and their validation

--- lib/sample_web/controllers/book_controller.ex
+++ lib/sample_web/controllers/book_controller.ex
@@ -1,8 +1,11 @@
 defmodule SampleWeb.BookController do
   use SampleWeb, :controller
 
-  alias OpenApiSpex.Operation
+  alias OpenApiSpex.{Operation, Schema}
   alias Sample.Schemas.BooksResponse
+
+  plug OpenApiSpex.Plug.Cast
+  plug OpenApiSpex.Plug.Validate
 
   @spec open_api_operation(any) :: Operation.t
   def open_api_operation(action), do: apply(__MODULE__, :"#{action}_operation", [])
@@ -15,6 +18,15 @@ defmodule SampleWeb.BookController do
       description: "Just a sample endpoint",
       operationId: "BookController.index",
       parameters: [
+        Operation.parameter(:filter, :query, %Schema{
+          type: :object,
+          required: [:published],
+          properties: %{
+            name: %Schema{description: "Book title", type: :string, example: "Encyclopedia"},
+            edition: %Schema{description: "Book edition", type: :integer, example: 2},
+            published: %Schema{description: "Book published date", type: :string, example: "2018-01-01", format: :date},
+          }
+        }, "Filters", style: "deepObject", explode: true, required: true)
       ],
       responses: %{
         200 => Operation.response("Books", "application/json", BooksResponse)
